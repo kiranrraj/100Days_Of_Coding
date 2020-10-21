e9b8a60acaa1101581913b449838dec71adbf3e3
Kiran Raj R <kiranrajvjd@gmail.com>
2020-10-21 18:47:58 +0530

    Day 7 updated

---
+++ Day_7/duplicate.py
@@ -0,0 +1,9 @@
+userStr1 = input("Enter your first string :")
+userStr2 = input("Enter your second string :")
+
+set_str_1 = set(userStr1)
+set_str_2 = set(userStr2)
+
+numMatchChar = set_str_1 & set_str_2
+print(f"Matched character is {numMatchChar}")
+print(f"Number of matched character is {len(numMatchChar)}")
