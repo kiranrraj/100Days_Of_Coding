53dc6c17f6dad3ac314b4ce4ea18bbb47eca8cbe
Kiran Raj R <kiranrajvjd@gmail.com>
2020-10-22 02:14:14 +0530

    Day 7 update

---
+++ Day_7/read_phone.py
@@ -0,0 +1,14 @@
+import re
+index = 1
+
+with open('phone_num.txt','r',encoding='utf-8') as file:
+     data = file.read()
+
+numformat = re.compile(r'\d\d\d\d \d\d\d\d\d\d\d')
+phonenums = numformat.findall(data)
+# findNum = numformat.search(data)
+# print(findNum.group())
+
+for phone in phonenums:
+    print(f"{index}.Phone number {phone}")
+    index+=1
