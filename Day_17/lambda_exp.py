b340344bd89d4cc673e1085ce801f84d8db8ea3d
Kiran Raj R <kiranrajvjd@gmail.com>
2020-10-31 23:55:54 +0530

    Day 17 updated

---
+++ Day_17/lambda_exp.py
@@ -0,0 +1,37 @@
+# Title  : Lambda example
+# Author : Kiran raj R.
+# Date   : 30:10:2020
+
+def higher_o_func(x, func): return x + func(x)
+
+
+result1 = higher_o_func(2, lambda x: x * x)
+# result1  x = 2, (2*2) + 2, x = 6
+
+result2 = higher_o_func(2, lambda x: x + 3)
+# result2 x=2, (2 + 3) + 2, x = 7
+
+result3 = higher_o_func(4, lambda x: x*x)
+# result3 = (4 * 4) + 4, x = 20
+
+result4 = higher_o_func(4, lambda x: x + 3)
+# result4 (4 + 3) + 4, x = 11
+
+print(result1, result2)
+print(result3, result4)
+
+
+# add = lambda x, y: x+y
+def add(x, y):
+    return x+y
+
+
+result5 = add(3, 4)
+print(result5)
+
+print((lambda x: x % 2 and 'odd' or 'even')(3))
+# odd
+
+# bool(2%2) => False
+# bool(2%3) => True
+#
---
+++ Day_17/map_in_python.py
@@ -0,0 +1,17 @@
+# Title  : Map function in python
+# Author : Kiran raj R.
+# Date   : 30:10:2020
+
+def getSquare(n):
+    return n * n
+
+
+numbers = [1, 2, 3, 4, 5]
+
+result = map(getSquare, numbers)
+square = set(result)
+
+print(square)
+
+result = list(map(lambda x: x+3, numbers))
+print(result)
