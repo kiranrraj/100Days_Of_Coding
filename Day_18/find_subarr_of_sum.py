8c7680294628a98919b9834b4963b0e900c0fa62
Kiran Raj R <kiranrajvjd@gmail.com>
2020-11-02 00:08:09 +0530

    Update find_subarr_of_sum.py

--- Day_18/find_subarr_of_sum.py
+++ Day_18/find_subarr_of_sum.py
@@ -14,16 +14,20 @@ def find_sub_array(list_in, sum_list):
 
     while list_end < length:
         if current_sum == sum_list:
-            print(
-                f"Sub array of sum {sum_list} is {list_in[list_start+1 : list_end+1]}")
-
-        if current_sum < sum_list:
+            break
+        elif current_sum < sum_list:
+            # print(list_end)
             list_end += 1
             current_sum += list_in[list_end]
         else:
             current_sum -= list_in[list_start]
             list_start += 1
-    return -1
+    return print(
+        f"Sub array of sum {sum_list} is {list_in[list_start : list_end+1]}")
 
 
 find_sub_array([1, 2, 3, 4, 5, 6, 7], 12)
+find_sub_array([1, 2, 3, 4, 5, 6, 7], 13)
+find_sub_array([1, 2, 3, 4, 5, 6, 7], 6)
+find_sub_array([1, 2, 3, 4, 5, 6, 7], 5)
+find_sub_array([1, 2, 3, 4, 5, 6, 7], 3)
