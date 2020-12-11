c3d3e98e35d573207ee0e90e1baf3c100c1d719c
Kiran Raj R <kiranrajvjd@gmail.com>
2020-12-11 23:53:25 +0530

    day_44_update_2

---
+++ Day_44/kth_column.py
@@ -0,0 +1,11 @@
+dia = int(input("Enter the diamension of the matrix: ")) 
+print(f"The dimension is {dia}") 
+result = [list(range(1 + dia * i, 1 + dia * (i + 1))) for i in range(dia)] 
+
+print(f"The Matrix is : {result}") 
+
+value_k = int(input("Enter the colum number: "))
+k_column = [sub[value_k] for sub in result] 
+  
+# printing result 
+print(f"The Kth column of matrix is : {k_column}") 
