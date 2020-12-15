d515a6f9fc86e3e1c236091af29957a4710314a5
Kiran Raj R <kiranrajvjd@gmail.com>
2020-12-15 23:19:25 +0530

    Day 47 update

---
+++ Day_47/circle.py
@@ -0,0 +1,10 @@
+PI = 3.14
+radius, area, circum = 0.0, 0.0, 0.0
+
+radius = float(input("Enter the radius of the circle: "))
+area = PI * radius ** 2
+circum = 2 * PI * radius
+
+print(f"Diameter of circle with radius {radius}: {radius * 2}")
+print(f"Area of circle with radius {radius}: {area}")
+print(f"Circumference of circle with radius {radius}: {circum:.2f}")
---
+++ Day_47/equilateral_tri.py
@@ -0,0 +1,13 @@
+import math
+side, area, perimeter, semi_p, altitude = 0.0, 0.0, 0.0, 0.0, 0.0
+
+side = float(input("Enter a side of an triangle: "))
+
+area = ( math.sqrt(3) / 4) * (side **2)
+perimeter = 3 * side
+altitude = (math.sqrt(3) / 2) * side
+
+print(f"Perimeter of a equilateral triangle with side, {side}: {perimeter:.2f}")
+print(f"Semi Perimeter of a equilateral triangle with side, {side}: {(perimeter / 2):.2f}")
+print(f"Area of a equilateral triangle with side {side}: {area:.2f}")
+print(f"Altitude of a equilateral triangle with side {side}: {altitude:.2f}")
