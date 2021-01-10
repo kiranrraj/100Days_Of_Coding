9816bb9022fa7703d8c372a4ff3bab8cd1028c29
Kiran Raj R <kiranrajvjd@gmail.com>
2021-01-10 22:02:37 +0530

    Day 69 update

---
+++ Day_69/py_countDown.py
@@ -0,0 +1,15 @@
+# Title  : Count down
+# Author : Kiran raj R.
+# Date   : 
+
+user_input = input("Enter a number to count down :")
+
+def countDown(x):
+    if x==0:
+        print("Lift off")
+    else:
+        print(x)
+        countDown(x-1)
+
+intVal = int(user_input)
+countDown(intVal)
